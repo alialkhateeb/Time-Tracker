fe07d542567172a17b41cbb65973026329f70cd6
Ali Alkhateeb <alialkhateeb@u.boisestate.edu>
2017-04-19 18:23:01 -0600

    change inddex

--- index.php
+++ index.php
@@ -7,10 +7,23 @@
 
 		<div class="contant">
 
-            <div class="frontPage"><p>Hello Everyone,
-            This website is a project for CS401- Web Development, and it is a tool that help student to study better and more effective.
-            Using timer to track the time you have spend studying, and it also give you a chance to record some ideas you might have while studying.
-          </p></div>
+        <div class="frontPage">
+            <pre>
+              This website is a project for CS401- Web Development,
+               and it is a tool that help student to study better and more
+               effective by recording their:
+                  - task name
+                  - number of hours it was spent on it
+                  - if you are done with the task
+                  - some of the thoughts came to you while studying
+
+              Using timer to track the time you have spend studying, and it
+              also give you a chance to record some ideas you might have while
+              studying.
+              
+              You can always come back and see what ever you have done.
+            </pre>
+        </div>
 
 
         </div>
